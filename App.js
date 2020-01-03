5c2cd00082919248492c9756b9310b84b414a570
Jackson Oppenheim <jacksonopp@gmail.com>
2020-01-03 10:46:37 -0600

    added dependencies, stack nav, context component, and static blog posts

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+import IndexScreen from './src/screens/IndexScreen'
+import { BlogProvider } from './src/context/BlogContext'
+
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+})
+
+const App = createAppContainer(navigator)
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <Text>{item.title}</Text>
+          )
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default IndexScreen
