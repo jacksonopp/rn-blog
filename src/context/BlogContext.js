34bc4194094a31d76e7482a0dba5ae52d0d836c0
Jackson Oppenheim <jacksonopp@gmail.com>
2020-01-04 14:20:36 -0600

    refactored context to use generic, added delete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,29 +1,27 @@
-import React, { useReducer } from 'react';
+import createDataContext from './createDataContext'
 
-const BlogContext = React.createContext()
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blog_post':
-      return [...state, { title: `Blog Post #${state.length + 1}` }]
+      return [...state, { id: Math.floor(Math.random() * 999999999), title: `Blog Post #${state.length + 1}` }]
+    case 'delete_blog_post':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
     default:
       return state;
   }
 }
 
-export const BlogProvider = ({ children }) => {
-
-  const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-  const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+  return () => {
     dispatch({ type: 'add_blog_post' })
   }
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  )
 }
 
-export default BlogContext;
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blog_post', payload: id })
+  }
+}
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [])
